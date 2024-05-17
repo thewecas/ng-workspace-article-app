8647d152ec3be831fe09fc8a69d8cf4f2f8782cc
vikas.m <vikas.m@pacewisdom.com>
2024-05-17 14:50:42 +0530

    Implement PWA for User-app

--- projects/admin-app/src/app/app.routes.ts
+++ projects/admin-app/src/app/app.routes.ts
@@ -3,21 +3,20 @@ import { ListArticlesComponent } from '@shared-lib';
 import { ArticleFormComponent } from './article-form/article-form.component';
 
 export const routes: Routes = [
-    
     {
         path: '',
-        redirectTo: 'article',
-        pathMatch: 'full'
-    },
-    {
-        path: 'article',
         component: ListArticlesComponent
     }, {
-        path: 'article/view/:id',
+        path: 'view/:id',
         component: ArticleFormComponent
     },
     {
-        path: 'article/new',
+        path: 'new',
         component: ArticleFormComponent
+    }
+    ,
+    {
+        path: '**',
+        redirectTo: '',
     }
 ];
--- projects/user-app/src/app/app.config.ts
+++ projects/user-app/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()),]
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), provideServiceWorker('ngsw-worker.js', {
+        enabled: !!isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
